0b932b7187ccf4cae1cdfb7ecb7924e51425529d
Micah Njeru <micahnjeru1@gmail.com>
2023-06-08 10:55:17 +0300

    Initial commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Micah Jesse Njeru on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
